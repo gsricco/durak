1cfc4e7813aae3a95a09f9f53ba4be6baf51b8db
oleg <olegpustovalov220@gmail.com>
2022-10-26 13:17:51 +0300

    add url and html

--- start_all_template/urls.py
+++ start_all_template/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-
-from .views import index
+from .views import index, index123, bonus_currency, contact, faq, honesty, profil
 
 urlpatterns = [
     path('', index, name='index'),
+    path('index123', index123, name='index123'),
+    path('bonus_currency', bonus_currency, name='bonus_currency'),
+    path('contact', contact, name='contact'),
+    path('faq', faq, name='faq'),
+    path('honesty', honesty, name='honesty'),
+    path('profil', profil, name='profil'),
 ]
