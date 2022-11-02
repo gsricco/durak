f5465c10f2927e0bcb6ff6cc3e44edf8682900d4
oleg <olegpustovalov220@gmail.com>
2022-11-03 00:29:29 +0300

    edit app edd forms.py

--- content_manager/views.py
+++ content_manager/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from social_django.models import UserSocialAuth
 
-from accaunts.models import DetailUser, Level
+from accaunts.forms import UserEditName
+from accaunts.models import DetailUser, Level, CustomUser, UserAgent
 from .models import FAQ, SiteContent
 
 
@@ -103,7 +104,21 @@ def honesty(request):
 def profil(request):
     """ПРОФИЛЬ"""
     sitecontent = SiteContent.objects.all()
+    # form_user = UserEditName(request.POST or None)
+    # print(request.method, 'methodsa')
+    # if request.method == 'POST':
+    #     print(request.POST, '123123')
+    #     if form_user.is_valid():
+    #         print(form_user.data, 'штаффыв')
+    #         form_user.save()
+    #         return render(request, 'profil.html')
+    # else:
+    #     print('OK')
     if request.user.is_authenticated:
+        agent = (request.META['HTTP_USER_AGENT'])
+        us = CustomUser.objects.get(username=request.user)
+        user_agent, created = UserAgent.objects.get_or_create(user=us, useragent=agent)
+        print(user_agent)
         detail_user = DetailUser.objects.get(user_id=request.user.id)
         level_data = Level.objects.get(pk=detail_user.level_id)
         if UserSocialAuth.objects.filter(user_id=request.user.id, provider='google-oauth2'):
@@ -121,6 +136,7 @@ def profil(request):
             "faq": faq,
             'social_google': social_google,
             'social_vk': social_vk,
+            # 'form_user': form_user,
         }
     else:
         level_data = Level.objects.get(level=1)
@@ -128,6 +144,22 @@ def profil(request):
             'sitecontent': sitecontent,
             "faq": faq,
             'level_data': level_data,
+            # 'form_user': form_user,
         }
     return render(request, 'profil.html', context)
 
+
+def update_profil(request):
+    form_user = UserEditName(request.POST or None)
+    print(form_user.data, 'methodsa')
+    if request.method == 'POST':
+        print(form_user, '++++')
+        if form_user.is_valid():
+            form_user.save()
+            return render(request, 'test.html')
+    else:
+        print('OK')
+    context = {
+        'form_user': form_user,
+    }
+    return render(request, 'test.html', context)
