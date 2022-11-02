93f55c64d064073132c72ee8e3be7c79c17e0ece
Anatoliy Dobshikov <anatoliy.dobshikov@gmail.com>
2022-11-02 20:22:03 +0300

    add some almost implemented views

--- caseapp/api_views.py
+++ caseapp/api_views.py
@@ -1,27 +1,53 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
-from .serializers import CaseSerializer, ItemSerializer
-from .models import Case, Item, ItemForCase
+from .serializers import CaseSerializer, ItemSerializer, OwnedCaseSerializer
+from .models import Case, Item, ItemForCase, OwnedCase
+
+from random import choices
 
 
-@api_view(['POST'])
+@api_view(['GET'])
 def open_case(request, owned_case_id):
     """Opens owned case and returns dropped item"""
-    pass
+    if not request.user.is_authenticated:
+        return Response(status=401)
+
+    owned_case = OwnedCase.objects.get(pk=owned_case_id)
+
+    if owned_case is None:
+        return Response({"details": f"cannot find case with id={owned_case_id}"}, status=404)
+
+    if owned_case.owner != request.user.pk:
+        return Response({"details": "Access forbidden"}, status=403)
+
+    case = owned_case.case
+    case_items = ItemForCase.objects.filter(case=case)
+
+    weights = case_items.values('chance')
+    chosen_item_in_case = choices(case_items, weights=weights, k=1)[0]
+    chosen_item = Item.objects.get(pk=chosen_item_in_case.item)
+
+    serializer = ItemSerializer(chosen_item)
+
+    return Response(serializer.data)
 
 
 @api_view(['GET'])
 def get_user_cases(request):
     """Returns cases for the user"""
-    pass
+    if not request.user.is_authenticated:
+        return Response(status=401)
+
+    cases = OwnedCase.objects.filter(owner=request.user)
+    serializer = OwnedCaseSerializer(cases, many=True)
+
+    return Response(serializer.data)
 
 
 @api_view(['GET'])
 def get_items_in_case(request, case_pk=None):
-    """
-    Returns list of items for the case
-    """
+    """Returns list of items for the case"""
     items_for_case = ItemForCase.objects.filter(case=case_pk)
     items = Item.objects.filter(pk__in=items_for_case.values('item'))
     serializer = ItemSerializer(items, many=True)
@@ -39,28 +65,24 @@ def get_items(request):
 
 @api_view(['GET'])
 def get_case_by_id(request, pk=None):
-    """
-    Returns specific case
-    """
-    if request.user.is_authenticated:
-        cases = Case.objects.all()
-        case = get_object_or_404(cases, pk=pk)
-        serializer = CaseSerializer(case)
-        return Response(serializer.data)
+    """Returns specific case"""
+    if not request.user.is_authenticated:
+        return Response(status=401)
 
-    return Response(status=401)
+    cases = Case.objects.all()
+    case = get_object_or_404(cases, pk=pk)
+    serializer = CaseSerializer(case)
+
+    return Response(serializer.data)
 
 
 @api_view(['GET'])
 def get_cases(request):
-    """
-    Returns cases for authenticated user
-    """
-    if request.user.is_authenticated:
-        cases = Case.objects.all()
-        serializer = CaseSerializer(cases, many=True)
-        return Response(serializer.data)
+    """Returns cases for authenticated user"""
+    if not request.user.is_authenticated:
+        return Response(status=401)
 
-    return Response(status=401)
+    cases = Case.objects.all()
+    serializer = CaseSerializer(cases, many=True)
 
-
+    return Response(serializer.data)
