2dec72a59358fa72f0d1cc6cb18020406a4fe2a3
Anatoliy Dobshikov <anatoliy.dobshikov@gmail.com>
2022-11-02 18:14:09 +0300

    add urls to api to work with cases

--- caseapp/urls.py
+++ caseapp/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .api_views import get_cases, get_case_by_id
+from .api_views import (
+    get_cases,
+    get_case_by_id,
+    get_items,
+    get_items_in_case,
+)
 
 urlpatterns = [
     path('cases/', get_cases, name='api_cases'),
-    path('cases/<int:pk>', get_case_by_id, name='api_case'),
-
+    path('cases/<int:pk>/', get_case_by_id, name='api_case'),
+    path('items/', get_items, name='api_items'),
+    path('items_for_case/<int:case_pk>/', get_items_in_case, name='api_items_in_case'),
 ]
