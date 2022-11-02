728285fe7d49f972d9cf01075aa8738e306ea19f
Anatoliy Dobshikov <anatoliy.dobshikov@gmail.com>
2022-11-02 13:43:36 +0300

    create api_views for getting cases

--- caseapp/api_views.py
+++ caseapp/api_views.py
@@ -1,11 +1,34 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 from .serializers import CaseSerializer
 from .models import Case
 
 
-class CaseListAPIView(ListAPIView):
-    """API View for getting cases on profile page"""
-    serializer_class = CaseSerializer
+@api_view(['GET'])
+def get_case_by_id(request, pk=None):
+    """
+    Returns specific case
+    """
+    if request.user.is_authenticated:
+        cases = Case.objects.all()
+        case = get_object_or_404(cases, pk=pk)
+        serializer = CaseSerializer(case)
+        return Response(serializer.data)
 
-    def get_queryset(self):
-        return Case.objects.all()
+    return Response(status=401)
+
+
+@api_view(['GET'])
+def get_cases(request):
+    """
+    Returns cases for authenticated user
+    """
+    if request.user.is_authenticated:
+        cases = Case.objects.all()
+        serializer = CaseSerializer(cases, many=True)
+        return Response(serializer.data)
+
+    return Response(status=401)
+
+
