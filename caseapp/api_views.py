ddcdb977e7e2e539367dcc5e4ef5ec1ae21a1363
Anatoliy Dobshikov <anatoliy.dobshikov@gmail.com>
2022-11-02 21:13:10 +0300

    implement views for getting user cases and for open case

--- caseapp/api_views.py
+++ caseapp/api_views.py
@@ -1,3 +1,5 @@
+import datetime
+
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
@@ -18,15 +20,22 @@ def open_case(request, owned_case_id):
     if owned_case is None:
         return Response({"details": f"cannot find case with id={owned_case_id}"}, status=404)
 
-    if owned_case.owner != request.user.pk:
+    if owned_case.owner.pk != request.user.pk:
         return Response({"details": "Access forbidden"}, status=403)
+
+    if owned_case.item is not None:
+        return Response({"details": "Case is already opened"}, status=403)
 
     case = owned_case.case
     case_items = ItemForCase.objects.filter(case=case)
 
-    weights = case_items.values('chance')
+    weights = [float(item['chance']) for item in case_items.values('chance')]
     chosen_item_in_case = choices(case_items, weights=weights, k=1)[0]
-    chosen_item = Item.objects.get(pk=chosen_item_in_case.item)
+    chosen_item = Item.objects.get(pk=chosen_item_in_case.item.pk)
+
+    owned_case.item = chosen_item
+    owned_case.date_opened = datetime.datetime.now()
+    owned_case.save()
 
     serializer = ItemSerializer(chosen_item)
 
@@ -39,7 +48,7 @@ def get_user_cases(request):
     if not request.user.is_authenticated:
         return Response(status=401)
 
-    cases = OwnedCase.objects.filter(owner=request.user)
+    cases = OwnedCase.objects.filter(owner=request.user.pk)
     serializer = OwnedCaseSerializer(cases, many=True)
 
     return Response(serializer.data)
