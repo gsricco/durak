fa940485f0c0cf3c3f93660a21573857a00edf1f
Anatoliy Dobshikov <anatoliy.dobshikov@gmail.com>
2022-11-02 21:13:36 +0300

    add open case and get user cases urls

--- caseapp/urls.py
+++ caseapp/urls.py
@@ -4,6 +4,8 @@ from .api_views import (
     get_case_by_id,
     get_items,
     get_items_in_case,
+    open_case,
+    get_user_cases,
 )
 
 urlpatterns = [
@@ -11,4 +13,6 @@ urlpatterns = [
     path('cases/<int:pk>/', get_case_by_id, name='api_case'),
     path('items/', get_items, name='api_items'),
     path('items_for_case/<int:case_pk>/', get_items_in_case, name='api_items_in_case'),
+    path('get_user_cases/', get_user_cases, name='api_get_user_cases'),
+    path('open/<int:owned_case_id>', open_case, name="api_open_case"),
 ]
