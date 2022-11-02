703052950cdb076340ed9de5eea30d42e90def99
Anatoliy Dobshikov <anatoliy.dobshikov@gmail.com>
2022-11-02 18:13:47 +0300

    create views to work with cases on profile page

--- caseapp/api_views.py
+++ caseapp/api_views.py
@@ -1,8 +1,40 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
-from .serializers import CaseSerializer
-from .models import Case
+from .serializers import CaseSerializer, ItemSerializer
+from .models import Case, Item, ItemForCase
+
+
+@api_view(['POST'])
+def open_case(request, owned_case_id):
+    """Opens owned case and returns dropped item"""
+    pass
+
+
+@api_view(['GET'])
+def get_user_cases(request):
+    """Returns cases for the user"""
+    pass
+
+
+@api_view(['GET'])
+def get_items_in_case(request, case_pk=None):
+    """
+    Returns list of items for the case
+    """
+    items_for_case = ItemForCase.objects.filter(case=case_pk)
+    items = Item.objects.filter(pk__in=items_for_case.values('item'))
+    serializer = ItemSerializer(items, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def get_items(request):
+    """Returns list of all items"""
+    items = Item.objects.all()
+    serializer = ItemSerializer(items, many=True)
+
+    return Response(serializer.data)
 
 
 @api_view(['GET'])
