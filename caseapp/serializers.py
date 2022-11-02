18aff2cb50f52e06b799ef8e1fb1c03882cb7f3d
Anatoliy Dobshikov <anatoliy.dobshikov@gmail.com>
2022-11-02 18:13:09 +0300

    create Item serializer modify Grade serializer

--- caseapp/serializers.py
+++ caseapp/serializers.py
@@ -1,18 +1,29 @@
 from rest_framework import serializers
-from .models import Case, Grade
+from .models import Case, Grade, Item
 
 
 class GradeModelSerializer(serializers.ModelSerializer):
     """Grade serializer for Case serializer"""
     class Meta:
         model = Grade
-        fields = ('name', 'min_lvl')
+        fields = ('name', 'image', 'min_lvl')
 
 
 class CaseSerializer(serializers.Serializer):
     """Case serializer"""
+    id = serializers.IntegerField()
     name = serializers.CharField()
     grade = GradeModelSerializer()
 
     class Meta:
         model = Case
+
+
+class ItemSerializer(serializers.Serializer):
+    """Item serializer"""
+    name = serializers.CharField()
+    image = serializers.ImageField()
+    selling_price = serializers.IntegerField()
+
+    class Meta:
+        model = Item
