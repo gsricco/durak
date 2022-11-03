8d45ad34747175302a86dea436ecf5fd5eecd806
oleg <olegpustovalov220@gmail.com>
2022-11-03 13:56:59 +0300

    edit form

--- accaunts/forms.py
+++ accaunts/forms.py
@@ -3,8 +3,8 @@ from accaunts.models import CustomUser
 
 
 class UserEditName(forms.ModelForm):
-    username = forms.CharField(max_length=20, widget=forms.TextInput(
-        attrs={'class': "form__input form profile-settings__name", 'type': "text", }))
+    username = forms.CharField(max_length=20,) # widget=forms.TextInput(
+        # attrs={'class': "form__input form profile-settings__name", 'type': "text", }))
 
     class Meta:
         model = CustomUser
