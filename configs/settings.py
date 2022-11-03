6832456d4fdc02fdb90ce19c6bc67add0a9fba72
korneyka3000 <korneyburau@gmail.com>
2022-11-03 15:44:29 +0300

    postgres db

--- configs/settings.py
+++ configs/settings.py
@@ -87,23 +87,23 @@ WSGI_APPLICATION = 'configs.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'durak_roll',
-#         'USER': 'durak',
-#         'PASSWORD': 'durak',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'durak_roll',
+        'USER': 'durak',
+        'PASSWORD': 'durak',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
