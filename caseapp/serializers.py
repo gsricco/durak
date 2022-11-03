ad62542d051cfccd789e57c13e2464ce817337d3
Anatoliy Dobshikov <anatoliy.dobshikov@gmail.com>
2022-11-03 20:11:55 +0300

    create serializer for checking whether user can open any case or not

--- caseapp/serializers.py
+++ caseapp/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import Case, Grade, Item, OwnedCase
-
+import datetime
 
 class GradeModelSerializer(serializers.ModelSerializer):
     """Grade serializer for Case serializer"""
@@ -35,6 +35,34 @@ class OwnedCaseSerializer(serializers.Serializer):
     date_owned = serializers.DateTimeField()
     date_opened = serializers.DateTimeField()
     item = ItemSerializer()
+
+    class Meta:
+        model = OwnedCase
+
+
+class OwnedCaseTimeSerializer(serializers.Serializer):
+    """Serializer for getting time before next case can be opened"""
+    date_opened = serializers.DateTimeField()
+    seconds_since_prev_open = serializers.SerializerMethodField()
+    can_be_opened = serializers.SerializerMethodField()
+
+    def get_seconds_since_prev_open(self, obj):
+        """Returns time since prev_open"""
+        if obj.date_opened is None:
+            return 0
+
+        delta_time = datetime.datetime.now(datetime.timezone.utc) - obj.date_opened
+        return delta_time.seconds
+
+    def get_can_be_opened(self, obj):
+        """Returns whether case can be opened or not"""
+        if obj.date_opened is None:
+            return True
+
+        delta_time = datetime.datetime.now(datetime.timezone.utc) - obj.date_opened
+        can_be = delta_time >= datetime.timedelta(hours=1)
+
+        return can_be
 
     class Meta:
         model = OwnedCase
