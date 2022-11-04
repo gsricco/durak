2f96a0fa2727a2fe9485d16329d73ae0582d4d80
oleg <olegpustovalov220@gmail.com>
2022-11-04 17:39:44 +0300

    edit admin.

--- accaunts/apps.py
+++ accaunts/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class AccauntsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accaunts'
+    verbose_name = "Информация пользователей сайта"
