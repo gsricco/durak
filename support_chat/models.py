ad0676d084eee69f21d8ad25e9352fa48132c87f
Mikl-Kenko <mikhail.kenko@gmail.com>
2022-11-04 19:55:31 +0200

    upupdate views,serializer and models(for work without adminpath)  in support_chat

--- support_chat/models.py
+++ support_chat/models.py
@@ -2,15 +2,17 @@ from django.contrib.auth.models import User
 from django.db import models
 from accaunts.models import CustomUser
 
+def admin():
+    return CustomUser.objects.get(username="admin").id
 
 class Message(models.Model):
     """Модель сообщений чата поддержки"""
     user_posted = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='user_send')
-    user_received = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='user_received')
+    user_received = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='user_received', default=admin())
     message = models.TextField(blank=True, null=True)
     file_message = models.FileField(upload_to=f'support_chat/%Y/%m/%d/', blank=True, null=True)
     date = models.DateTimeField(auto_now_add=True)
-    is_read = models.BooleanField(default=False)
+    is_read = models.BooleanField(default=False, blank=True, null=True)
 
     def __str__(self):
         return self.message
