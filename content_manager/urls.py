fe32b7ed0342a77923094e50fe0eb9ec323276e6
korneyka3000 <korneyburau@gmail.com>
2022-11-10 14:39:56 +0300

    template base url change, urls change, gitignore added migrations

--- content_manager/urls.py
+++ content_manager/urls.py
@@ -4,8 +4,8 @@ from .views import index, bonus_currency, contact, faq, honesty, profil
 urlpatterns = [
     path('', index, name='index'),
     path('bonus-currency', bonus_currency, name='bonus-currency'),
-    path('contact', contact, name='contact'),
-    path('faq', faq, name='faq'),
-    path('honesty', honesty, name='honesty'),
-    path('profil', profil, name='profil'),
+    path('contact/', contact, name='contact'),
+    path('faq/', faq, name='faq'),
+    path('honesty/', honesty, name='honesty'),
+    path('profil/', profil, name='profil'),
 ]
