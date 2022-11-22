97f331fec75a09693c6b0bad3f6df50d5d33434a
Tobyret2 <s.malishevsky@yandex.ru>
2022-11-22 19:42:55 +0300

    add view username for admin chat

--- ws_chat/consumers.py
+++ ws_chat/consumers.py
@@ -2,24 +2,27 @@ import json
 from asgiref.sync import sync_to_async, async_to_sync
 from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.layers import get_channel_layer
+import redis
+
 from accaunts.models import CustomUser
 from support_chat.models import Message, UserChatRoom
 from support_chat.serializers import RoomSerializer
 
+r = redis.Redis()
 
 class ChatConsumer(AsyncWebsocketConsumer):
-
+    user_reciever = ''
     @sync_to_async
-    def create_or_get_support_chat_room(self, message):  # получает или создает чат рум
-        if message.get('user'):
-            chat_room, created = UserChatRoom.objects.get_or_create(room_id=message.get('user'))
+    def create_or_get_support_chat_room(self, user):  # получает или создает чат рум
+
+            chat_room, created = UserChatRoom.objects.get_or_create(room_id=user)
             return chat_room
 
     @sync_to_async
-    def save_user_message(self, room, message):  # сохраняет сообщение в бд
-        if message.get('user'):
-            user = CustomUser.objects.get(username=message.get('user')).pk
-            user_mess = Message(user_posted_id=user, message=message.get('message'))
+    def save_user_message(self, room, user , message):  # сохраняет сообщение в бд
+        if user:
+            user = CustomUser.objects.get(username=user).pk
+            user_mess = Message(user_posted_id=user, message=message)
             user_mess.save()
             room.message.add(user_mess, bulk=False)
             return user_mess
@@ -30,43 +33,57 @@ class ChatConsumer(AsyncWebsocketConsumer):
             room_data = UserChatRoom.objects.get(room_id=room_name)
             serializer = RoomSerializer(room_data)
             channel_layer = get_channel_layer()
-            for i in serializer.data.get('message')[::-1]:
+            for i in serializer.data.get('message'):
                 async_to_sync(channel_layer.send)(channel, {"type": "chat_message",
                                                             "chat_type": "support",
                                                             "message": i['message'],
-                                                            "user": i['user_posted'],
+                                                            "user": i['user_posted']['username'],
                                                             })
         except:
             pass
     async def connect(self):
-        user = self.scope['user']
+        recieve_user = self.scope["url_route"]["kwargs"]["user"]
+        user = str(self.scope['user'])
+        print(f'Польщователь {user} подключился ')
+        r.set(user,self.channel_name)
         self.room_name = 'go'  # self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = "chat_%s" % self.room_name
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
         await self.accept()
         await self.send_online(0)
-        await self.send_json(self.channel_name, user)
+        if recieve_user == 'go':
+            await self.send_json(self.channel_name, user)
+        else:
+            await self.send_json(self.channel_name,recieve_user)
+
 
     async def send_online(self, num):
         online = self.channel_layer.receive_count + num
         await self.channel_layer.group_send(self.room_group_name, {"type": "get_online", "get_online": online})
 
     async def disconnect(self, code):
+        r.delete(str(self.scope['user']))
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
         await self.send_online(-1)
 
     async def receive(self, text_data=None, bytes_data=None):
         text_data_json = json.loads(text_data)
-
         if text_data_json.get('chat_type') == 'support':
-            room = await self.create_or_get_support_chat_room(text_data_json)
-            await self.save_user_message(room, text_data_json)
+            room = await self.create_or_get_support_chat_room(text_data_json.get('user'))
+            await self.save_user_message(room,str(self.scope['user']), text_data_json["message"])
             await self.channel_layer.send(
                 self.channel_name, {"type": "chat_message",
                                        "chat_type": "support",
                                        "message": text_data_json["message"],
                                        "user": text_data_json["user"], })
+            byte_user_channel_name = r.get('admin')
+            user_channel_name = bytes.decode(byte_user_channel_name, encoding='utf-8')
 
+            await self.channel_layer.send(
+                user_channel_name, {"type": "chat_message",
+                                       "chat_type": "support",
+                                       "message": text_data_json["message"],
+                                       "user": text_data_json["user"], })
         elif text_data_json.get('chat_type') == 'all_chat':
             message = text_data_json["message"]
             user = text_data_json["user"]
@@ -80,8 +97,28 @@ class ChatConsumer(AsyncWebsocketConsumer):
                                        "user": user,
                                        "avatar": avatar,
                                        "rubin": rubin,
-                                       }
-            )
+                                       })
+        elif text_data_json.get('chat_type') == 'support_admin':
+            byte_user_channel_name = r.get(text_data_json.get('user_receiver'))
+            user_channel_name = bytes.decode(byte_user_channel_name,encoding='utf-8')
+            if user_channel_name:
+                user = text_data_json.get('user_receiver')
+                self.user_reciever = user
+                room = await self.create_or_get_support_chat_room(user)
+                await self.save_user_message(room , user, text_data_json['message'])
+                print(self.scope['user'],'наш юзер')
+                await self.channel_layer.send(
+                    user_channel_name, {"type": "chat_message",
+                                        "chat_type": "support",
+                                        "message": text_data_json["message"],
+                                        "user": str(self.scope['user']) })
+
+                await self.channel_layer.send(
+                    self.channel_name, {"type": "chat_message",
+                                        "chat_type": "support",
+                                        "message": text_data_json["message"],
+                                        "user": str(self.scope['user'])})
+
 
     async def get_online(self, event):
         # print(event['get_online'])
