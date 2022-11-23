a5411bcfe029accfd116d3edc728bacc57999db0
korneyka3000 <korneyburau@gmail.com>
2022-11-23 17:16:30 +0300

    requirements.txt changed celery works well some changes in front

--- configs/celery.py
+++ configs/celery.py
@@ -1,5 +1,9 @@
+import json
 import os
 import asyncio
+import time
+
+import redis
 from celery import Celery, shared_task
 from channels.layers import get_channel_layer
 from celery.schedules import crontab
@@ -7,18 +11,17 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'configs.settings')
 app = Celery('configs')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
-from asgiref.sync import async_to_sync
+from asgiref.sync import async_to_sync, sync_to_async
 
 channel_layer = get_channel_layer()
 
 
 @app.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
-    sender.add_periodic_task(30.03, debug_task.s(), name='add every 10')
+    sender.add_periodic_task(30.03, debug_task.s(), name='add every 30')
 
-@shared_task(bind=True)
-def sender(self):
-    print(self)
+@shared_task
+def sender():
     async_to_sync(channel_layer.group_send)('chat_go',
                                             {
                                                 'type': 'korney_task',
@@ -27,17 +30,38 @@ def sender(self):
                                             )
     print('konec')
     return {"my_name": "Korney"}
-# @app.task
+
 @shared_task
 def debug_task():
     sender.apply_async()
-    stop.apply_async(countdown=20)
+    roll.apply_async(countdown=20)
+    stop.apply_async(countdown=25)
+    go_back.apply_async(countdown=28)
 
-@shared_task(bind=True)
-def stop(self):
-    print(self)
+@shared_task
+def roll():
     async_to_sync(channel_layer.group_send)('chat_go',
                                             {
-                                                'type':'stopper',
+                                                'type': 'rolling',
+                                            })
+@shared_task
+def stop():
+    # print(self.__dict__)
+    async_to_sync(channel_layer.group_send)('chat_go',
+                                            {
+                                                'type': 'stopper',
 
                                             })
+@shared_task
+def go_back():
+    async_to_sync(channel_layer.group_send)('chat_go',
+                                            {
+                                                'type': 'go_back',
+                                            })
+
+
+# @shared_task
+# def debug_task():
+#     redis_client = redis.StrictRedis(host='localhost', port=6379, db=1)
+#     json_data = {'message': 'Hello to all connected clients', 'date': '2019-02-02', 'title': 'welcome'}
+#     redis_client.publish('roulette', json.dumps(json_data))
--- ws_chat/consumers.py
+++ ws_chat/consumers.py
@@ -1,6 +1,9 @@
 import json
+
+import redis
 from channels.generic.websocket import AsyncWebsocketConsumer
 # from configs.celery import debug_task
+
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -12,6 +15,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
         # a = debug_task.apply_async()
         await self.accept()
 
+
     async def disconnect(self, code):
         await self.channel_layer.group_discard(
             self.room_group_name, self.channel_name
@@ -20,6 +24,26 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data=None, bytes_data=None):
         text_data_json = json.loads(text_data)
         print(text_data_json)
+        # if text_data_json.get('get') == 'message':
+        #     # await self.send_message()
+        #     r = redis.StrictRedis(host='localhost', port=6379, db=1)
+        #     p = r.pubsub()
+        #     p.psubscribe('roulette')
+        #     print('start sending message')
+        #     for message in p.listen():
+        #         if message:
+        #             m_type = message.get('type', '')
+        #             m_pattern = message.get('pattern', '')
+        #             m_channel = message.get('channel', '')
+        #             data = message.get('data', '')
+        #             print(data, 'nasha data')
+        #             await self.channel_layer.group_send(
+        #                 self.room_group_name, {
+        #                     "type": "korney_task",
+        #                     "data": data
+        #                 })
+        #
+        #         break
         if text_data_json.get('online') == "online":
             online = self.channel_layer.receive_count
             await self.channel_layer.group_send(
@@ -28,7 +52,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
                                        }
             )
         else:
-            message = text_data_json["message"]
+            message = text_data_json.get("message")
             user = text_data_json["user"]
             avatar = text_data_json["avatar"]
             rubin = text_data_json.get("rubin")
@@ -41,9 +65,26 @@ class ChatConsumer(AsyncWebsocketConsumer):
                                        "rubin": rubin,
                                        }
             )
-        # self.send(text_data=json.dumps({'message': message}))
+    async def send_message(self):
+        r = redis.StrictRedis(host='localhost', port=6379, db=1)
+        p = r.pubsub()
+        p.subscribe('roulette')
+        print('start sending message')
+        for message in p.listen():
+            if message:
+                m_type = message.get('type', '')
+                m_pattern = message.get('pattern', '')
+                m_channel = message.get('channel', '')
+                data = message.get('data', '')
+                print(data, 'nasha data')
+                await self.channel_layer.group_send(
+                    self.room_group_name, {
+                        "type": "korney_task",
+                        "data": data
+                    })
+
+            break
     async def get_online(self, event):
-        print(event['get_online'])
         online = event['get_online']
         await self.send(text_data=json.dumps({
             "get_online": online
@@ -62,16 +103,24 @@ class ChatConsumer(AsyncWebsocketConsumer):
                                               }))
 # начинает отсчёт
     async def korney_task(self, event):
+        data = event.get('data')
         await self.send(text_data=json.dumps({
             "roulette": 20,
+            "data": data
         }))
 # начинает крутиться рулетка
     async def rolling(self, event):
         await self.send(text_data=json.dumps({
-            "roll": 'rolling'
+            "roll": 'rolling',
         }))
 
     async def stopper(self, event):
+        print(event)
         await self.send(text_data=json.dumps({
-            "stop": "stopping"
+            "stop": "stopping",
+        }))
+
+    async def go_back(self, event):
+        await self.send(text_data=json.dumps({
+            'back': 'go-back',
         }))
