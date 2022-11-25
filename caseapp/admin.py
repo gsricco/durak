ac5596e6bf06f98b32b153abbe685ae1de0417c4
Anatoliy Dobshikov <anatoliy.dobshikov@gmail.com>
2022-11-25 11:22:37 +0300

    Add rewards to user from admin

--- caseapp/admin.py
+++ caseapp/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Case, OwnedCase, Item, ItemForCase, Grade
+from .models import Case, OwnedCase, Item, ItemForCase, Grade, Reward, GivenReward
 
 admin.site.register(Case)
 admin.site.register(OwnedCase)
 admin.site.register(Item)
 admin.site.register(ItemForCase)
 admin.site.register(Grade)
+admin.site.register(Reward)
+admin.site.register(GivenReward)
--- caseapp/models.py
+++ caseapp/models.py
@@ -6,7 +6,7 @@ class Item(models.Model):
     Model stores information about an item
     """
     name = models.CharField(verbose_name='Название', max_length=255)
-    image = models.ImageField(verbose_name='Изображение', upload_to='items')
+    image = models.ImageField(verbose_name='Изображение', upload_to='items', null=True, blank=True)
     selling_price = models.PositiveIntegerField(verbose_name='Цена продажи', default=0)
     chance_price = models.PositiveIntegerField(verbose_name='Цена для расчёта шансов', default=0)
     is_money = models.BooleanField(verbose_name='Предмет является кредитами', default=False)
@@ -28,7 +28,7 @@ class Grade(models.Model):
     min_lvl = models.PositiveIntegerField(verbose_name='Минимальный уровень')
 
     def __str__(self):
-        return self.name
+        return f"{self.name}, минимальный уровень: {self.min_lvl}"
 
     class Meta:
         verbose_name = 'Уровень кейса'
@@ -61,7 +61,7 @@ class ItemForCase(models.Model):
     case = models.ForeignKey('Case', verbose_name='Кейс', on_delete=models.PROTECT)
 
     def __str__(self):
-        return f"{self.item.name} from {self.case.name}"
+        return f"<{self.item.name}> из кейса <{self.case.name}>"
 
     class Meta:
         verbose_name = 'Предмет в кейсе'
@@ -74,15 +74,72 @@ class OwnedCase(models.Model):
     """
     case = models.ForeignKey('Case', verbose_name='Кейс', on_delete=models.PROTECT)
 
-    owner = models.ForeignKey('accaunts.CustomUser', on_delete=models.PROTECT)
+    owner = models.ForeignKey('accaunts.CustomUser', verbose_name='Владелец', on_delete=models.PROTECT)
 
     date_owned = models.DateTimeField(verbose_name='Дата получения', auto_now_add=True)
     date_opened = models.DateTimeField(verbose_name='Дата открытия', null=True, blank=True)
     item = models.ForeignKey('Item', verbose_name='Выпавший предмет', on_delete=models.PROTECT, null=True, blank=True)
 
     def __str__(self):
-        return f"{self.case.name} {self.pk} for {self.owner}"
+        return f"{self.case.name}<{self.pk}> для {self.owner}"
 
     class Meta:
-        verbose_name = 'Кейс пользователя'
-        verbose_name_plural = 'Кейсы пользователей'
+        verbose_name = 'Выданный кейс'
+        verbose_name_plural = 'Выданные кейсы'
+
+
+class Reward(models.Model):
+    level = models.ForeignKey('accaunts.Level', verbose_name='Уровень', on_delete=models.CASCADE)
+    case = models.ForeignKey('Case', verbose_name='Кейс', on_delete=models.CASCADE)
+    amount = models.PositiveIntegerField(verbose_name='Количество кейсов', default=1)
+
+    def __str__(self) -> str:
+        return f"Кейс <{self.case.name}> ({self.amount} ед.) на уровне {self.level.level}"
+
+    class Meta:
+        verbose_name = 'Награды на уровне'
+        verbose_name_plural = 'Награды на уровнях'
+
+
+class GivenReward(models.Model):
+    reward = models.ForeignKey('Reward', verbose_name='Награда на уровне', on_delete=models.PROTECT)
+    user = models.ForeignKey('accaunts.CustomUser', verbose_name='Пользователь', on_delete=models.CASCADE)
+    date = models.DateTimeField(verbose_name='Дата выдачи', auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        # метод save не вызывается, когда объекты создаются через bulk_create
+        # при добавлении награды за уровень из админки, сразу произойдёт начисление кейсов
+        self.give_rewards(save_immediately=True)
+        return super().save(*args, **kwargs)
+
+    def give_rewards(self, save_immediately: bool=False) -> list:
+        """Начисляет связанному пользователю награду из связанного объекта Reward
+
+        Args:
+            save_immediately (bool, optional): Сохранять начисленные кейсы в БД внутри метода или нет. Defaults to False.
+
+        Returns:
+            list: список с объектами начисленных кейсов
+        """
+        # Список с начисленными кейсами
+        owned_cases = []
+        for _ in range(self.reward.amount):
+            # создаёт начисление кейса и закидывает в список
+            new_owned_case = OwnedCase(case=self.reward.case, owner=self.user)
+            owned_cases.append(new_owned_case)
+
+        print(f"Give reward to user: {self}")
+
+        # сохраняет начисленные кейсы в БД
+        if save_immediately and owned_cases:
+            OwnedCase.objects.bulk_create(owned_cases)
+
+        # возвращает начисленные кейсы (если их нужно будет сохранить позже, в одном bulk_create с другими, например)
+        return owned_cases
+
+    def __str__(self):
+        return f"Награда <{self.reward}> для пользователя <{self.user.pk}>:<{self.date}>"
+
+    class Meta:
+        verbose_name = 'Выданная награда за уровень'
+        verbose_name_plural = 'Выданные награды за уровни'
