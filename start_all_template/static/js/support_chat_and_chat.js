7a5ce91ebbfcbf320c55f426515fc31358201a6e
Tobyret2 <s.malishevsky@yandex.ru>
2022-12-06 10:52:45 +0300

    изменил цвет фона сообщений в чате поддержки

--- start_all_template/static/js/support_chat_and_chat.js
+++ start_all_template/static/js/support_chat_and_chat.js
@@ -69,7 +69,7 @@ const newUserMessage = (message, user, file_path) => {
         li.style.justifyContent = 'flex-start'
         li.style.paddingLeft = '0%'
         li.style.paddingRight = '16%'
-        div.style.background = 'orange'
+        div.style.background = '#2f2f2f'
         div.style.borderRadius = '15px 15px 15px 0px'
         spanUser.style.textAlign = 'left'
     }
