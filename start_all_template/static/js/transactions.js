c634ccb89b1c21c3c62b1abd2cbe0fd7a72c186e
Anatoliy Dobshikov <anatoliy.dobshikov@gmail.com>
2022-12-22 12:26:24 +0300

    change tabs behavior

--- start_all_template/static/js/transactions.js
+++ start_all_template/static/js/transactions.js
@@ -1,5 +1,13 @@
+const tabs_link_target = document.querySelectorAll("a.profil-main__tabs-link");
+// остаёмся на профиле при клике на вкладку
+tabs_link_target[0].addEventListener("click", function() {
+    const url = new URL(document.location);
+    const searchParams = url.searchParams;
+    searchParams.delete("page");
+    window.history.pushState({}, '', url.toString());
+})
+// переключаемся на транзакции при загрузке страницы
 window.addEventListener("load", function () {
-    const tabs_link_target = document.querySelectorAll("a.profil-main__tabs-link")[1];
     let click_event = new Event("click", {bubbles: true, cancelable: true, composed: true});
-    tabs_link_target.dispatchEvent(click_event);
+    tabs_link_target[1].dispatchEvent(click_event);
 })
