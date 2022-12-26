5ffca7a2ae082a24e487ba21754b584bab22496f
korneyka3000 <korneyburau@gmail.com>
2022-12-26 17:40:51 +0300

    robokassa app deleted

--- robokassa_pay/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Popoln
-
-
-@admin.register(Popoln)
-class PopolnAdmin(admin.ModelAdmin):
-    list_display = 'id', 'sum'
--- robokassa_pay/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class RobokassaPayConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "robokassa_pay"
