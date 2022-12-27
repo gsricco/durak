4fb722522c35b83440bfcf879b7349e7be1677b9
Tobyret2 <s.malishevsky@yandex.ru>
2022-12-27 14:47:28 +0300

    подфиксил админку , исправил баг с лвлами примиграциях , исправил отображение баланса , прописал verbos_name

--- start_all_template/static/js/script.js
+++ start_all_template/static/js/script.js
@@ -1235,18 +1235,26 @@ const static_prefix = JSON.parse(document.getElementById('static_prefix').textCo
 
 
 let UserBalancer = Number(document.querySelector('#userBal').textContent)
-
-if (UserBalancer / 1000 > 9 && UserBalancer / 1000 < 1000) {
-    UserBalancerShow = `${UserBalancer / 1000}K`
-} else {
-    if (UserBalancer / 1000000 > 0) {
-        UserBalancerShow = `${UserBalancer / 1000000}M`
-    } else
-        UserBalancerShow = `${UserBalancer}`
-}
-if (UserBalancer / 1000 > 0 && UserBalancer / 1000 < 10) {
-    UserBalancerShow = `${UserBalancer}`
-}
+if (UserBalancer < 1000000) {
+    let nal = UserBalancer / 1000
+    if (UserBalancer % 1000 === 0){
+        UserBalancerShow = nal+'K'
+    }else{
+        let strList = String(nal).split('.')
+        UserBalancerShow = `${strList[0]}.${strList[1].slice(0,1)}K`
+}}else {
+    //если больше 1млн
+    let nal = UserBalancer / 1000000
+    let strList = String(nal).split('.')
+    if (UserBalancer % 1000000 === 0) {
+        UserBalancerShow = nal+'М'
+    }else if(UserBalancer < 10000000){
+        UserBalancerShow = `${strList[0]}.${strList[1].slice(0,2)}M`
+    }else if(UserBalancer >= 10000000 && UserBalancer < 100000000){
+        UserBalancerShow = `${strList[0]}.${strList[1].slice(0,1)}M`
+    }else if(UserBalancer > 100000000 ) {
+        UserBalancerShow = `${strList[0]}M`
+}}
 
 document.querySelector('#userBal').innerHTML = `${UserBalancerShow}`
 document.querySelector('#userBalMob').innerHTML = `${UserBalancerShow}`
