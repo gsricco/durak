7fbc6e96cd59e6897d81b1e5996a99a9cdfcaf90
korneyka3000 <korneyburau@gmail.com>
2022-12-28 13:53:43 +0300

    some fix for balance if user is Anonymous

--- start_all_template/static/js/script.js
+++ start_all_template/static/js/script.js
@@ -1233,24 +1233,24 @@ window.addEventListener("DOMContentLoaded", () => {
 const media_prefix = JSON.parse(document.getElementById('media_prefix').textContent);
 const static_prefix = JSON.parse(document.getElementById('static_prefix').textContent);
 
+if (is_auth) {
+    let UserBalancer = Number(document.querySelector('#userBal').textContent)
 
-let UserBalancer = Number(document.querySelector('#userBal').textContent)
-
-if (UserBalancer / 1000 > 9 && UserBalancer / 1000 < 1000) {
-    UserBalancerShow = `${UserBalancer / 1000}K`
-} else {
-    if (UserBalancer / 1000000 > 0) {
-        UserBalancerShow = `${UserBalancer / 1000000}M`
-    } else
+    if (UserBalancer / 1000 > 9 && UserBalancer / 1000 < 1000) {
+        UserBalancerShow = `${UserBalancer / 1000}K`
+    } else {
+        if (UserBalancer / 1000000 > 0) {
+            UserBalancerShow = `${UserBalancer / 1000000}M`
+        } else
+            UserBalancerShow = `${UserBalancer}`
+    }
+    if (UserBalancer / 1000 > 0 && UserBalancer / 1000 < 10) {
         UserBalancerShow = `${UserBalancer}`
-}
-if (UserBalancer / 1000 > 0 && UserBalancer / 1000 < 10) {
-    UserBalancerShow = `${UserBalancer}`
-}
+    }
 
-document.querySelector('#userBal').innerHTML = `${UserBalancerShow}`
-document.querySelector('#userBalMob').innerHTML = `${UserBalancerShow}`
-
+    document.querySelector('#userBal').innerHTML = `${UserBalancerShow}`
+    document.querySelector('#userBalMob').innerHTML = `${UserBalancerShow}`
+}
 
 
 
