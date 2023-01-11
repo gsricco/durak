9b56c504efb6af01578cfd02c639f3b1f2f9a00c
korneyka3000 <korneyburau@gmail.com>
2023-01-11 04:08:19 +0300

    mw fixed

--- configs/middleware.py
+++ configs/middleware.py
@@ -29,11 +29,11 @@ def add_new_ip(get_response):
     def mw(request):
         if request.user.is_authenticated:
             # ip1 = (request.META['REMOTE_ADDR'])
-            ip2 = request.META.get("HTTP_X_REAL_IP")
-            # print(ip1, ip2)
-            if not request.user.userip_set.filter(userip=ip1).exists():
+            ip = request.META.get("HTTP_X_REAL_IP")
+            print(ip, 'ETO IP FROM HTTP_X_REAL_IP')
+            if not request.user.userip_set.filter(userip=ip).exists():
                 # print(request.user.userip_set.filter(userip=ip1).exists())
-                UserIP.objects.create(userip=ip1, user=request.user)
+                UserIP.objects.create(userip=ip, user=request.user)
                 # UserIP.objects.create(userip=ip2, user=request.user)
             response = get_response(request)
             return response
