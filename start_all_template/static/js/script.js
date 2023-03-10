2753815701325c375507e93f463879d290c1929f
winsert1 <96631693+winsert1@users.noreply.github.com>
2023-03-10 20:22:02 +0300

    Что такое кредиты

--- start_all_template/static/js/script.js
+++ start_all_template/static/js/script.js
@@ -11,6 +11,55 @@ window.addEventListener("DOMContentLoaded", () => {
     const MAIN = document.querySelector("main");
     const FOOTER = document.querySelector("footer");
 
+    // Что такое кредиты
+    var containers;
+function initDrawers() {
+	// Get the containing elements
+	containers = document.querySelectorAll(".container");
+	setHeights();
+	wireUpTriggers();
+	window.addEventListener("resize", setHeights);
+}
+
+window.addEventListener("load", initDrawers);
+
+function setHeights() {
+	containers.forEach(container => {
+		// Get content
+		let content = container.querySelector(".content");
+		// Needed if this is being fired after a resize
+		content.removeAttribute("aria-hidden");
+		// Height of content to show/hide
+		let heightOfContent = content.getBoundingClientRect().height;
+		// Set a CSS custom property with the height of content
+		container.style.setProperty("--containerHeight", `${heightOfContent}px`);
+		// Once height is read and set
+		setTimeout(e => {
+			container.classList.add("height-is-set");
+			content.setAttribute("aria-hidden", "true");
+		}, 0);
+	});
+}
+
+function wireUpTriggers() {
+	containers.forEach(container => {
+		// Get each trigger element
+		let btn = container.querySelector(".trigger");
+		// Get content
+		let content = container.querySelector(".content");
+		btn.addEventListener("click", () => {
+			container.setAttribute(
+				"data-drawer-showing",
+				container.getAttribute("data-drawer-showing") === "true" ? "false" : "true"
+			);
+			content.setAttribute(
+				"aria-hidden",
+				content.getAttribute("aria-hidden") === "true" ? "false" : "true"
+			);
+		});
+	});
+}
+    
     //Отображение смайла 25%
   if (document.querySelector(".amount-selection__form")) {
     let randomSmileNum = Math.floor(Math.random() * 4);
