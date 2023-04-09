0dbe4994bf630ae5f4b11cb53dd276e9d86c310c
korneyka3000 <korneyburau@gmail.com>
2023-04-09 23:24:11 +0300

    migrations stopped

--- start.sh
+++ start.sh
@@ -5,8 +5,8 @@
 #set -o nounset
 
 #
-python manage.py makemigrations
-python manage.py migrate
+#python manage.py makemigrations
+#python manage.py migrate
 python manage.py collectstatic --noinput
 #python manage.py runserver
 daphne -b 0.0.0.0 -p 8000 configs.asgi:application
