c8601f6c124262d61a815d70de951b2b458ba67b
korneyka3000 <korneyburau@gmail.com>
2023-04-29 13:40:21 +0300

    Update support_chatserializers.py -> new field added 'usernamegame'

--- support_chat/serializers.py
+++ support_chat/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
-from rest_framework.parsers import (FileUploadParser, FormParser,
-                                    MultiPartParser)
+from rest_framework.parsers import FileUploadParser, FormParser, MultiPartParser
 
 from accaunts.models import CustomUser
 
@@ -10,7 +9,7 @@ from .models import Message, UserChatRoom
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ('id', 'username', 'avatar')
+        fields = ("id", "username", "avatar")
 
 
 class MessageGetSerializer(serializers.Serializer):
@@ -28,17 +27,21 @@ class MessageGetSerializer(serializers.Serializer):
 #         parser_classes = (FileUploadParser,)
 #         fields = ('file_message',)
 
+
 class MessageCreateSerializer(serializers.ModelSerializer):
     # file_message = MessageFileCreateSerializer(default=None)
     class Meta:
         model = Message
-        fields = ('message', 'file_message')
+        fields = ("message", "file_message")
         ordering = [-1]
+
+
 class RoomMessageSerializer(serializers.ModelSerializer):
     user_posted = UserSerializer()
+
     class Meta:
-        model =  UserChatRoom
-        fields = ('message')
+        model = UserChatRoom
+        fields = "message"
 
 
 # class RoomAdminMessageSerializer(serializers.ModelSerializer):
@@ -46,20 +49,27 @@ class RoomMessageSerializer(serializers.ModelSerializer):
 #         model =  AdminMessage
 #         fields = ('user_posted','message')
 
+
 class RoomSerializer(serializers.ModelSerializer):
 
     message = MessageGetSerializer(many=True)
+
     class Meta:
         model = UserChatRoom
-        fields = ('room_id','message')
+        fields = ("room_id", "message")
+
 
 class NoReadSerializer(serializers.Serializer):
     not_read_counter = serializers.IntegerField()
     not_read_owner_counter = serializers.IntegerField()
+
+
 class UserFromRoomSerializer(serializers.Serializer):
     username = serializers.CharField()
+    usernamegame = serializers.CharField()
+
+
 class OnlyRoomSerializer(serializers.Serializer):
     user = UserFromRoomSerializer()
     room_id = serializers.CharField()
     notread = NoReadSerializer()
-
